3f976ed1462378f3309b069c0b18025d1de8b96b
IT diorderaja <it@diorderaja.co.id>
2020-11-25 17:39:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Title';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Title />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
